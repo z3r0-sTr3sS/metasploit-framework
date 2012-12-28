3608a15f5aa1d6264ff5cf2f34f5b04d67feb60b
Tod Beardsley <todb@metasploit.com>
2012-12-28 12:57:23 -0600

    Adding a shell wrapper around msfclone

---
+++ msfclone.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# This is an unattended version of https://gist.github.com/4393324
+# You must have rights to the /opt dir, which usually means root.
+
+MSFENV=/opt/metasploit*/scripts/setenv.sh
+if [ -f $MSFENV ];
+then
+	source $MSFENV
+fi
+rm -rf /opt/metasploit/msf3 
+mkdir -p /opt/metasploit/msf3
+curl -LO http://curl.haxx.se/ca/cacert.pem
+CURL_CA_BUNDLE=$PWD/cacert.pem curl -L http://r-7.co/UsEqxW > msfclone.rb
+MSFBASE=/opt/metasploit/msf3 MSFUNATTENDED=true ruby msfclone.rb
+
+
