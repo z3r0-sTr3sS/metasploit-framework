5aed7dfc1f9ff46de243b7cd5b6840e7ecdc00d0
Tod Beardsley <todb@metasploit.com>
2013-01-10 05:04:36 -0800

    Replace the existing msf dir

--- msfclone.sh
+++ msfclone.sh
@@ -7,11 +7,13 @@ MSFENV=/opt/metasploit*/scripts/setenv.sh
 if [ -f $MSFENV ];
 then
 	source $MSFENV
+	MSFBASE=dirname $MSFENV
+else
+        MSFBASE=/opt/metasploit/msf3
 fi
-rm -rf /opt/metasploit/msf3 
-mkdir -p /opt/metasploit/msf3
+rm -rf $MSFBASE
+mkdir -p $MSFBASE 
 \curl -LO http://curl.haxx.se/ca/cacert.pem
 CURL_CA_BUNDLE=$PWD/cacert.pem \curl -L http://r-7.co/UsEqxW > msfclone.rb
-MSFBASE=/opt/metasploit/msf3 MSFUNATTENDED=true ruby msfclone.rb
-
+MSFUNATTENDED=true ruby msfclone.rb
 
